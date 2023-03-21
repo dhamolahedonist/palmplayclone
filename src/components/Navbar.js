fc5b073906c13f95d7c4a99f83193b990fdc3fe8
dhamolahedonist <dhamolahedonist>
2023-03-21 20:42:35 +0100

    media query for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,95 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdArrowDropDown } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [showLinks, setshowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks]);
+
   return (
-    <nav className="navbar">
+    <nav>
       <div className="nav-center">
-        <Link>
-          <img
-            src="https://www.palmpay.com/_next/static/media/logo.5ef03e1a.png?w=256&q=100"
-            alt="palmpay"
-            className="logo"
-          />
-        </Link>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <Link to="/">
-            Personal
-            <span>
-              <MdArrowDropDown
-                style={{
-                  display: "inline-block",
-                  justiftyContent: "center",
-                  alignItems: "center",
-                  color: "#c4c4c4",
-                  fontSize: "1.2rem",
-                  verticalAlign: "middle",
-                }}
-              />
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">Agent </Link>
-        </li>
-        <li>
-          <Link to="/">
-            Company
-            <span>
-              <MdArrowDropDown
-                style={{
-                  display: "inline-block",
-                  justiftyContent: "center",
-                  alignItems: "center",
-                  color: "#c4c4c4",
-                  fontSize: "1.2rem",
-                  verticalAlign: "middle",
-                }}
-              />
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">Press </Link>
-        </li>
-        <li>
+        <div className="nav-header">
           <Link>
             <img
-              className="nav-flag"
-              src="https://www.palmpay.com/_next/static/media/NG.a8d189be.png?w=96&q=100"
+              src="https://www.palmpay.com/_next/static/media/logo.5ef03e1a.png?w=256&q=100"
               alt="palmpay"
+              className="logo"
             />
           </Link>
-        </li>
-      </ul>
+          <button
+            className="nav-toggle"
+            onClick={() => setshowLinks((prevState) => !prevState)}
+          >
+            {showLinks ? <FaBars /> : <FaTimes />}
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            <li>
+              <Link to="/">
+                Personal
+                <span>
+                  <MdArrowDropDown
+                    style={{
+                      display: "inline-block",
+                      justiftyContent: "center",
+                      alignItems: "center",
+                      color: "#c4c4c4",
+                      fontSize: "1.2rem",
+                      verticalAlign: "middle",
+                    }}
+                  />
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Agent </Link>
+            </li>
+            <li>
+              <Link to="/">
+                Company
+                <span>
+                  <MdArrowDropDown
+                    style={{
+                      display: "inline-block",
+                      justiftyContent: "center",
+                      alignItems: "center",
+                      color: "#c4c4c4",
+                      fontSize: "1.2rem",
+                      verticalAlign: "middle",
+                    }}
+                  />
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Press</Link>
+            </li>
+
+            <li>
+              <Link>
+                <img
+                  className="nav-flag"
+                  src="https://www.palmpay.com/_next/static/media/NG.a8d189be.png?w=96&q=100"
+                  alt="palmpay"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
